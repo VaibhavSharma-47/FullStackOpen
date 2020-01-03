7fbbbccf08fb71b763f23b2db46348bccdee22f9
unknown <17532va@gmail.com>
2020-01-03 21:53:54 +0530

    Anecdotes

---
+++ 1/1.14 anecdotes step3/src/index.js
@@ -0,0 +1,71 @@
+import React, { useState} from 'react'
+import ReactDOM from 'react-dom'
+const MostVotes =({anecdotes,votes}) =>{
+  console.log(Math.max(votes))
+  let max = 0
+  let index =0 
+
+  for(let s =0;s<anecdotes.length;s++){
+    if(votes[s]>max){
+      max=votes[s]
+      index = s
+    }
+  }
+  return(
+    <>
+    {anecdotes[index]}
+    <br></br>
+    has {votes[index]} votes
+    </>
+  )
+}
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
+  const [votes,setVotes] = useState({
+    0:0,
+    1:0,
+    2:0,
+    3:0,
+    4:0,
+    5:0}
+  )
+  const Votes=()=>{
+    const newvotes={
+      ...votes
+    }
+    newvotes[selected]+=1
+    setVotes(newvotes)
+    console.log(newvotes)
+  }
+  return (
+    
+    <div>
+      <h1>Anecdotes of the day</h1>
+      {props.anecdotes[selected]}
+      <br></br>
+      has {votes[selected]} votes
+      <br></br>
+
+      <button onClick={()=>{setSelected (Math.floor(Math.random()*(anecdotes.length)))}}>next anecdotes</button>
+      <button onClick={()=>Votes()}>Vote</button>
+      <h1>Anecdotes with most votes</h1>
+      <MostVotes anecdotes={anecdotes} votes={votes}></MostVotes>
+    </div>
+  )
+}
+
+
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
+  document.getElementById('root')
+)
--- 1/1.7 Unicafe step2/src/index.js
+++ 1/1.7 Unicafe step2/src/index.js
@@ -1,21 +1,7 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Statistic = ({good,neutral,bad}) => {
-  return(
-    <>
-        <h1>statistic</h1>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
-        <p>all {good + neutral + bad}</p>
-        
-        <p>average {(good- bad)/(3*(good + neutral + bad))}</p>
-        
-        <p>positive {(good*100)/((good + neutral + bad))} %</p>
-    </>
-  )
-}
+
 
 
 const App = () => {
@@ -36,7 +22,16 @@ const App = () => {
         <button onClick={()=>setBad(bad+1)}>
             bad
         </button>
-        <Statistic good = {good} neutral = {neutral} bad={bad}></Statistic>
+        <h1>statistic</h1>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+        <p>all {good + neutral + bad}</p>
+        
+        <p>average {(good- bad)/(3*(good + neutral + bad))}</p>
+        
+        <p>positive {(good*100)/((good + neutral + bad))} %</p>
+
     </div>
   )
 }
